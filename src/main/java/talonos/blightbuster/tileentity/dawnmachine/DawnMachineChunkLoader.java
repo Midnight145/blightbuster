d336a58db7e08271c454b24a306ec7ca1981d6b6
Ryan O'Dell <ryanodell@my.unt.edu>
2022-01-06 12:07:02 -0600

    update dawnmachinechunkloader to remove unneccesary func

--- src/main/java/talonos/blightbuster/tileentity/dawnmachine/DawnMachineChunkLoader.java
+++ src/main/java/talonos/blightbuster/tileentity/dawnmachine/DawnMachineChunkLoader.java
@@ -2,7 +2,6 @@ package talonos.blightbuster.tileentity.dawnmachine;
 
 import java.util.List;
 
-import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.world.World;
 import net.minecraftforge.common.ForgeChunkManager;
 import talonos.blightbuster.tileentity.DawnMachineTileEntity;
@@ -12,13 +11,5 @@ public class DawnMachineChunkLoader implements ForgeChunkManager.LoadingCallback
 	DawnMachineTileEntity dawnMachine;
 
 	@Override
-	public void ticketsLoaded(List<ForgeChunkManager.Ticket> tickets, World world) {
-		for (ForgeChunkManager.Ticket ticket : tickets) {
-			NBTTagCompound data = ticket.getModData();
-			String id = data.getString("id");
-			if (id == "DawnMachine") {
-//    			dawnMachine.forceChunkLoading(ticket); 
-			}
-		}
-	}
+	public void ticketsLoaded(List<ForgeChunkManager.Ticket> tickets, World world) {}
 }
