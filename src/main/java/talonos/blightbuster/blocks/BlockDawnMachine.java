05f13b8a665f199f0884aa76386dfbb362b56098
Midnight <rubikscubeboy27@gmail.com>
2022-03-01 00:35:40 -0600

    add onBlockActivated for divination sigil

--- src/main/java/talonos/blightbuster/blocks/BlockDawnMachine.java
+++ src/main/java/talonos/blightbuster/blocks/BlockDawnMachine.java
@@ -2,14 +2,18 @@ package talonos.blightbuster.blocks;
 
 import java.util.Random;
 
+import WayofTime.alchemicalWizardry.ModItems;
 import cpw.mods.fml.common.registry.GameRegistry;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.ChatComponentText;
 import net.minecraft.util.IIcon;
 import net.minecraft.world.World;
 import talonos.blightbuster.BBStrings;
@@ -20,72 +24,99 @@ import thaumcraft.common.Thaumcraft;
 
 public class BlockDawnMachine extends BlockMultiblock {
 
-    private IIcon top;
-    private IIcon bottom;
-    private IIcon sideLive;
-    private IIcon sideDead;
-    private IIcon glow;
+	private IIcon top;
+	private IIcon bottom;
+	private IIcon sideLive;
+	private IIcon sideDead;
+	private IIcon glow;
 
-    public BlockDawnMachine() {
-        super(Material.wood, BBBlock.dawnMachineMultiblock);
-        this.setLightLevel(.875f);
-        this.setHardness(10.0F);
-        this.setResistance(500.0F);
-        this.setBlockName(BlightBuster.MODID + "_" + BBStrings.dawnMachineName);
-        this.setStepSound(soundTypeWood);
-        GameRegistry.registerBlock(this, this.getUnlocalizedName());
-    }
+	public BlockDawnMachine() {
+		super(Material.wood, BBBlock.dawnMachineMultiblock);
+		this.setLightLevel(.875f);
+		this.setHardness(10.0F);
+		this.setResistance(500.0F);
+		this.setBlockName(BlightBuster.MODID + "_" + BBStrings.dawnMachineName);
+		this.setStepSound(soundTypeWood);
+		GameRegistry.registerBlock(this, this.getUnlocalizedName());
+	}
 
-    @Override
+	@Override
 	public boolean onBlockEventReceived(World par1World, int par2, int par3, int par4, int par5, int par6) {
-        if (par5 == 1) {
-            if (par1World.isRemote) {
-                Thaumcraft.proxy.blockSparkle(par1World, par2, par3, par4, 16736256, 5);
-            }
-            return true;
-        }
-        return super.onBlockEventReceived(par1World, par2, par3, par4, par5, par6);
-    }
+		if (par5 == 1) {
+			if (par1World.isRemote) {
+				Thaumcraft.proxy.blockSparkle(par1World, par2, par3, par4, 16736256, 5);
+			}
+			return true;
+		}
+		return super.onBlockEventReceived(par1World, par2, par3, par4, par5, par6);
+	}
 
-    @SideOnly(Side.CLIENT)
-    @Override
-    public void registerBlockIcons(IIconRegister registry) {
-        top = registry.registerIcon("blightbuster:dawnMachineTop");
-        sideLive = registry.registerIcon("blightbuster:dawnMachineSide");
-        sideDead = registry.registerIcon("blightbuster:dawnMachineDeactivated");
-        bottom = registry.registerIcon("blightbuster:dawnMachineBottom");
-        glow = registry.registerIcon("blightbuster:dawnMachineGlow");
-    }
+	@SideOnly(Side.CLIENT)
+	@Override
+	public void registerBlockIcons(IIconRegister registry) {
+		this.top = registry.registerIcon("blightbuster:dawnMachineTop");
+		this.sideLive = registry.registerIcon("blightbuster:dawnMachineSide");
+		this.sideDead = registry.registerIcon("blightbuster:dawnMachineDeactivated");
+		this.bottom = registry.registerIcon("blightbuster:dawnMachineBottom");
+		this.glow = registry.registerIcon("blightbuster:dawnMachineGlow");
+	}
 
-    @SideOnly(Side.CLIENT)
-    @Override
-    public int getRenderType() { return -1; }
+	@SideOnly(Side.CLIENT)
+	@Override
+	public int getRenderType() { return -1; }
 
-    public IIcon getTopIcon() { return top; }
-    public IIcon getBottomIcon() { return bottom; }
-    public IIcon getLiveSideIcon() { return sideLive; }
-    public IIcon getDeadSideIcon() { return sideDead; }
-    public IIcon getGlowIcon() { return glow; }
+	public IIcon getTopIcon() { return this.top; }
 
-    @Override
-    public Item getItemDropped(int meta, Random par2Random, int par3) {
-        return Item.getItemFromBlock(BBBlock.cyberTotem);
-    }
+	public IIcon getBottomIcon() { return this.bottom; }
 
-    @Override
-    public boolean hasTileEntity(int meta) {
-        return true;
-    }
+	public IIcon getLiveSideIcon() { return this.sideLive; }
 
-    @Override
-    public TileEntity createTileEntity(World world, int meta) {
-        return new DawnMachineTileEntity();
-    }
+	public IIcon getDeadSideIcon() { return this.sideDead; }
 
-    @Override
-    public void breakBlock(World world, int x, int y, int z, Block block, int meta) {
-        super.breakBlock(world, x, y, z, block, meta);
+	public IIcon getGlowIcon() { return this.glow; }
+
+	@Override
+	public Item getItemDropped(int meta, Random par2Random, int par3) {
+		return Item.getItemFromBlock(BBBlock.cyberTotem);
+	}
+
+	@Override
+	public boolean hasTileEntity(int meta) {
+		return true;
+	}
+
+	@Override
+	public TileEntity createTileEntity(World world, int meta) {
+		return new DawnMachineTileEntity();
+	}
+
+	@Override
+	public void breakBlock(World world, int x, int y, int z, Block block, int meta) {
+		super.breakBlock(world, x, y, z, block, meta);
 
 //        BlightBuster.instance.chunkLoader.eliminateDawnMachine();
-    }
+	}
+
+	@Override
+	public boolean onBlockActivated(World world, int x, int y, int z, EntityPlayer player, int idk, float what,
+			float these, float are) {
+		DawnMachineTileEntity tileEntity = (DawnMachineTileEntity) world.getTileEntity(x, y, z);
+
+		if (tileEntity == null) {
+			return false;
+		}
+
+		ItemStack playerItem = player.getCurrentEquippedItem();
+
+		if (playerItem != null && playerItem.getItem().equals(ModItems.divinationSigil) && !player.isSneaking()) {
+			if (!player.worldObj.isRemote) {
+				player.addChatMessage(new ChatComponentText("Current Blood: " + tileEntity.getFluidAmount()));
+				player.addChatMessage(new ChatComponentText("Current RF: " + tileEntity.getEnergyStored()));
+				player.addChatMessage(new ChatComponentText("Current Mana: " + tileEntity.getCurrentMana()));
+			}
+			return true;
+		}
+
+		return false;
+	}
 }
