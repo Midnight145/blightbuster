86ba03940f075149dd8cbbf02cee8d8795712442
Midnight <23142346+Midnight145@users.noreply.github.com>
2022-07-18 14:41:55 -0500

    formatting, chunkload while running, no longer see flesh as taint

--- src/main/java/talonos/biomescanner/BiomeScanner.java
+++ src/main/java/talonos/biomescanner/BiomeScanner.java
@@ -18,6 +18,7 @@ import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
 import net.minecraft.nbt.CompressedStreamTools;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraftforge.common.ForgeChunkManager;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.common.config.Configuration;
 import talonos.biomescanner.block.BSBlock;
@@ -28,99 +29,99 @@ import talonos.biomescanner.map.MapScanner;
 import talonos.biomescanner.map.Zone;
 
 @Mod(modid = BiomeScanner.MODID, name = BiomeScanner.MODNAME, version = BiomeScanner.VERSION, dependencies = BiomeScanner.DEPS)
-public class BiomeScanner
-{
-	
+public class BiomeScanner {
+
 	public static final String MODID = "biomescanner";
 	public static final String MODNAME = "Biome Scanner";
-    public static final String VERSION = "1.2.19";
-    public static final String DEPS = "before:UndergroundBiomes;after:ThermalFoundation;after:appliedenergistics2;after:Thaumcraft";
-	public static final String COMMONPROXYLOCATION = "talonos."+MODID+".CommonProxy";
-	public static final String CLIENTPROXYLOCATION = "talonos."+MODID+".ClientProxy";
+	public static final String VERSION = "1.2.19";
+	public static final String DEPS = "before:UndergroundBiomes;after:ThermalFoundation;after:appliedenergistics2;after:Thaumcraft";
+	public static final String COMMONPROXYLOCATION = "talonos." + MODID + ".CommonProxy";
+	public static final String CLIENTPROXYLOCATION = "talonos." + MODID + ".ClientProxy";
 
-    public static File baselineFile;
+	public static File baselineFile;
 
-    public static final CreativeTabs badgesTab = new CreativeTabs("badges")
-    {
-        private static final String __OBFID = "CL_00000011";
-        @SideOnly(Side.CLIENT)
-        public Item getTabIconItem()
-        {
-            return BSItems.badge;
-        }
-    };
-	
+	public static final CreativeTabs badgesTab = new CreativeTabs("badges") {
+		private static final String __OBFID = "CL_00000011";
+
+		@Override
+		@SideOnly(Side.CLIENT)
+		public Item getTabIconItem() { return BSItems.badge; }
+	};
+
 	@SidedProxy(clientSide = BiomeScanner.CLIENTPROXYLOCATION, serverSide = BiomeScanner.COMMONPROXYLOCATION)
 	public static CommonProxy proxy;
 
-    @Mod.Instance(BiomeScanner.MODID)
+	@Mod.Instance(BiomeScanner.MODID)
 	public static BiomeScanner instance;
 
-    public static Integer[] zoneBaselines = new Integer[Zone.values().length];
+	public static Integer[] zoneBaselines = new Integer[Zone.values().length];
 
-    public static boolean disableEverything = false;
+	public static boolean disableEverything = false;
 
 	@Mod.EventHandler
-    public static void preInit(FMLPreInitializationEvent event)
-    {
+	public static void preInit(FMLPreInitializationEvent event) {
 		BSBlock.init();
 		BSItems.init();
 		proxy.registerTileEntities();
 
-        File configFile = event.getSuggestedConfigurationFile();
-        Configuration config = new Configuration(configFile);
+		File configFile = event.getSuggestedConfigurationFile();
+		Configuration config = new Configuration(configFile);
 
-        boolean useBaselineFile = config.get(Configuration.CATEGORY_GENERAL, "useBaselineFile", false).getBoolean();
-        String baselineFileName = config.get(Configuration.CATEGORY_GENERAL, "baselineFileName", "baseline.dat").getString();
-        disableEverything = config.get(Configuration.CATEGORY_GENERAL, "disableEverything", false).getBoolean();
-        config.save();
+		boolean useBaselineFile = config.get(Configuration.CATEGORY_GENERAL, "useBaselineFile", false).getBoolean();
+		String baselineFileName = config.get(Configuration.CATEGORY_GENERAL, "baselineFileName", "baseline.dat")
+				.getString();
+		disableEverything = config.get(Configuration.CATEGORY_GENERAL, "disableEverything", false).getBoolean();
+		config.save();
 
-        for (int i = 0; i < zoneBaselines.length; i++)
-            zoneBaselines[i] = null;
+		for (int i = 0; i < zoneBaselines.length; i++) {
+			zoneBaselines[i] = null;
+		}
 
-        if (useBaselineFile) {
-            BiomeScanner.baselineFile = new File(configFile.getParent(), baselineFileName);
-            if (baselineFile.exists()) {
-                try {
-                    NBTTagCompound loadedData = CompressedStreamTools.readCompressed(new FileInputStream(baselineFile));
-                    loadBaselineData(loadedData);
-                } catch (IOException ex) {
-                    ex.printStackTrace();
-                }
-            }
-        }
-    }
+		if (useBaselineFile) {
+			BiomeScanner.baselineFile = new File(configFile.getParent(), baselineFileName);
+			if (baselineFile.exists()) {
+				try {
+					NBTTagCompound loadedData = CompressedStreamTools.readCompressed(new FileInputStream(baselineFile));
+					loadBaselineData(loadedData);
+				} catch (IOException ex) {
+					ex.printStackTrace();
+				}
+			}
+		}
+	}
 
-    private static void loadBaselineData(NBTTagCompound loadedData) {
-        NBTTagCompound regionMap = loadedData.getCompoundTag("RegionMap");
-        NBTTagCompound baseline = regionMap.getCompoundTag("BaselineBlocks");
+	private static void loadBaselineData(NBTTagCompound loadedData) {
+		NBTTagCompound regionMap = loadedData.getCompoundTag("RegionMap");
+		NBTTagCompound baseline = regionMap.getCompoundTag("BaselineBlocks");
 
-        for (int i = 0; i < zoneBaselines.length; i++) {
-            if (baseline.hasKey(Integer.toString(i)))
-                zoneBaselines[i] = baseline.getInteger(Integer.toString(i));
-            else
-                zoneBaselines[i] = null;
-        }
+		for (int i = 0; i < zoneBaselines.length; i++) {
+			if (baseline.hasKey(Integer.toString(i))) {
+				zoneBaselines[i] = baseline.getInteger(Integer.toString(i));
+			}
+			else {
+				zoneBaselines[i] = null;
+			}
+		}
 
-    }
- 
-    @Mod.EventHandler
-    public static void init(FMLInitializationEvent event)
-    {
-        FMLCommonHandler.instance().bus().register(MapScanner.instance);
-        MinecraftForge.EVENT_BUS.register(MapScanner.instance);
-    }
- 
-    @Mod.EventHandler
-    public static void postInit(FMLPostInitializationEvent event)
-    {
-        BiomeMapColors.initColors();
-        proxy.registerRenderers();
-        NetworkRegistry.INSTANCE.registerGuiHandler(instance, new GuiHandlerBadgePrinter());
-    }
+	}
 
-    @Mod.EventHandler
-    public static void serverLoad(FMLServerStartingEvent event) {
-        event.registerServerCommand(new ResetBaselineCommand());
-    }
+	@Mod.EventHandler
+	public void init(FMLInitializationEvent event) {
+		FMLCommonHandler.instance().bus().register(MapScanner.instance);
+		MinecraftForge.EVENT_BUS.register(MapScanner.instance);
+
+		ForgeChunkManager.setForcedChunkLoadingCallback(this, MapScanner.instance); // adds chunkloader to forge
+	}
+
+	@Mod.EventHandler
+	public static void postInit(FMLPostInitializationEvent event) {
+		BiomeMapColors.initColors();
+		proxy.registerRenderers();
+		NetworkRegistry.INSTANCE.registerGuiHandler(instance, new GuiHandlerBadgePrinter());
+	}
+
+	@Mod.EventHandler
+	public static void serverLoad(FMLServerStartingEvent event) {
+		event.registerServerCommand(new ResetBaselineCommand());
+	}
 }
--- src/main/java/talonos/biomescanner/block/BlockScannerController.java
+++ src/main/java/talonos/biomescanner/block/BlockScannerController.java
@@ -19,119 +19,122 @@ import talonos.biomescanner.map.MapScanner;
 import talonos.biomescanner.tileentity.TileEntityIslandMapper;
 import talonos.biomescanner.tileentity.TileEntityIslandScanner;
 
-public class BlockScannerController extends BSBlock implements ITileEntityProvider
-{
-    private IIcon back;
-    private IIcon front;
-    private IIcon top;
-    private IIcon bottom;
-    private IIcon sides;
+public class BlockScannerController extends BSBlock implements ITileEntityProvider {
+	private IIcon back;
+	private IIcon front;
+	private IIcon top;
+	private IIcon bottom;
+	private IIcon sides;
 
-	public BlockScannerController() 
-	{
-		this.setBlockName(BiomeScanner.MODID+"_"+ BSStrings.blockScannerControllerName);
+	public BlockScannerController() {
+		this.setBlockName(BiomeScanner.MODID + "_" + BSStrings.blockScannerControllerName);
 		this.setBlockUnbreakable();
 		this.setResistance(6000000.0F);
 		this.setStepSound(soundTypePiston);
 		this.disableStats();
 		this.setCreativeTab(CreativeTabs.tabBlock);
-        this.setLightLevel(0.16f);
+		this.setLightLevel(0.16f);
 		GameRegistry.registerBlock(this, this.getUnlocalizedName());
 	}
-	      
+
 	@SideOnly(Side.CLIENT)
-    @Override
-	public void registerBlockIcons(IIconRegister par1IconRegister)
-	{
-        back = par1IconRegister.registerIcon("biomescanner:controller-back");
-        front = par1IconRegister.registerIcon("biomescanner:controller-front");
-        sides = par1IconRegister.registerIcon("biomescanner:controller-sides");
-        bottom = par1IconRegister.registerIcon("biomescanner:controller-bottom");
-        top = par1IconRegister.registerIcon("biomescanner:controller-top");
-        this.blockIcon = top;
+	@Override
+	public void registerBlockIcons(IIconRegister par1IconRegister) {
+		this.back = par1IconRegister.registerIcon("biomescanner:controller-back");
+		this.front = par1IconRegister.registerIcon("biomescanner:controller-front");
+		this.sides = par1IconRegister.registerIcon("biomescanner:controller-sides");
+		this.bottom = par1IconRegister.registerIcon("biomescanner:controller-bottom");
+		this.top = par1IconRegister.registerIcon("biomescanner:controller-top");
+		this.blockIcon = this.top;
 	}
 
-    @SideOnly(Side.CLIENT)
-    @Override
-    public IIcon getIcon(int side, int meta)
-    {
-        if (side == 0)
-            return bottom;
-        else if (side == 1)
-            return top;
-        else if (side == 2)
-            return front;
-        else if (side == 3)
-            return back;
-        else
-            return sides;
-    }
-	
-    /**
-     * Is this block (a) opaque and (b) a full 1m cube?  This determines whether or not to render the shared face of two
-     * adjacent blocks and also whether the player can attach torches, redstone wire, etc to this block.
-     */
+	@SideOnly(Side.CLIENT)
 	@Override
-    public boolean isOpaqueCube()
-    {
-        return false;
-    }
+	public IIcon getIcon(int side, int meta) {
+		if (side == 0) {
+			return this.bottom;
+		}
+		else if (side == 1) {
+			return this.top;
+		}
+		else if (side == 2) {
+			return this.front;
+		}
+		else if (side == 3) {
+			return this.back;
+		}
+		else {
+			return this.sides;
+		}
+	}
+
+	/**
+	 * Is this block (a) opaque and (b) a full 1m cube? This determines whether or
+	 * not to render the shared face of two
+	 * adjacent blocks and also whether the player can attach torches, redstone
+	 * wire, etc to this block.
+	 */
+	@Override
+	public boolean isOpaqueCube() { return false; }
 
 	@Override
-	public TileEntity createNewTileEntity(World world, int iDunno) 
-	{
+	public TileEntity createNewTileEntity(World world, int iDunno) {
 		return new TileEntityIslandScanner();
 	}
 
-    @Override
-    public int onBlockPlaced(World world, int x, int y, int z, int side, float hitX, float hitY, float hitZ, int metadata) {
-        for (int iterX = 0; iterX <= 4; iterX++)
-        {
-            for (int iterY = 0; iterY <= 6; iterY++)
-            {
-                TileEntity hopefullyAMap = world.getTileEntity(x + iterX - 2, y + iterY, z + 7);
+	@Override
+	public int onBlockPlaced(World world, int x, int y, int z, int side, float hitX, float hitY, float hitZ,
+			int metadata) {
+		MapScanner.instance.chunkX = Math.floorDiv(x, 16);
+		MapScanner.instance.chunkZ = Math.floorDiv(z, 16);
+		MapScanner.instance.world = world;
+		for (int iterX = 0; iterX <= 4; iterX++) {
+			for (int iterY = 0; iterY <= 6; iterY++) {
+				TileEntity hopefullyAMap = world.getTileEntity(x + iterX - 2, y + iterY, z + 7);
 
-                if (hopefullyAMap instanceof TileEntityIslandMapper)
-                {
-                    TileEntityIslandMapper map = (TileEntityIslandMapper) hopefullyAMap;
+				if (hopefullyAMap instanceof TileEntityIslandMapper) {
+					TileEntityIslandMapper map = (TileEntityIslandMapper) hopefullyAMap;
 
-                    map.setMapCoords(iterX* MapScanner.blockWidth, (5-iterY)*MapScanner.blockHeight);
+					map.setMapCoords(iterX * MapScanner.blockWidth, (5 - iterY) * MapScanner.blockHeight);
 
-                    world.markBlockForUpdate(map.xCoord, map.yCoord, map.zCoord);
-                }
-            }
-        }
+					world.markBlockForUpdate(map.xCoord, map.yCoord, map.zCoord);
+				}
+			}
+		}
 
-        return metadata;
-    }
+		return metadata;
+	}
 
-    @Override
-    public boolean onBlockActivated(World world, int x, int y, int z, EntityPlayer player, int side, float hitX, float hitY, float hitZ) {
-        if (ForgeDirection.VALID_DIRECTIONS[side] == ForgeDirection.UP) {
-            if (!world.isRemote) {
-                if (MapScanner.instance.isActive()) {
-                    player.addChatComponentMessage(new ChatComponentTranslation("scanner.gui.alreadyactive"));
-                } else {
-                    MapScanner.instance.activate();
-                    TileEntityIslandScanner scanner = ((TileEntityIslandScanner)world.getTileEntity(x,y,z));
-                    if (scanner != null)
-                        scanner.scanOrdered();
-                }
-            }
-            return true;
-        } else {
-            if (world.getTileEntity(x, y, z) != null && !player.isSneaking()) {
-                player.openGui(BiomeScanner.instance, 0, world, x, y, z);
-                return true;
-            }
-        }
+	@Override
+	public boolean onBlockActivated(World world, int x, int y, int z, EntityPlayer player, int side, float hitX,
+			float hitY, float hitZ) {
+		if (ForgeDirection.VALID_DIRECTIONS[side] == ForgeDirection.UP) {
+			if (!world.isRemote) {
+				if (MapScanner.instance.isActive()) {
+					player.addChatComponentMessage(new ChatComponentTranslation("scanner.gui.alreadyactive"));
+				}
+				else {
+					MapScanner.instance.activate();
+					TileEntityIslandScanner scanner = ((TileEntityIslandScanner) world.getTileEntity(x, y, z));
+					if (scanner != null) { scanner.scanOrdered(); }
+				}
+			}
+			return true;
+		}
+		else {
+			if (world.getTileEntity(x, y, z) != null && !player.isSneaking()) {
+				player.openGui(BiomeScanner.instance, 0, world, x, y, z);
+				return true;
+			}
+		}
 
-        return false;
-    }
+		return false;
+	}
 
-    @Override
-    public void breakBlock(World world, int x, int y, int z, Block block, int side) {
-        if (world.getTileEntity(x,y,z) != null)
-            ((TileEntityIslandScanner)world.getTileEntity(x,y,z)).unregister();
-    }
+	@Override
+	public void breakBlock(World world, int x, int y, int z, Block block, int side) {
+		if (world.getTileEntity(x, y, z) != null) {
+			((TileEntityIslandScanner) world.getTileEntity(x, y, z)).unregister();
+		}
+	}
 }
