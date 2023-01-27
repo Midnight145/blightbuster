e684945931f806af21e19c02233e5ab7d4e171cb
Midnight <23142346+Midnight145@users.noreply.github.com>
2023-01-27 14:11:34 -0600

    increment version number, update mcmod.info

--- src/main/java/talonos/biomescanner/BiomeScanner.java
+++ src/main/java/talonos/biomescanner/BiomeScanner.java
@@ -3,6 +3,7 @@ package talonos.biomescanner;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.Arrays;
 
 import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.common.Mod;
@@ -30,69 +31,71 @@ import talonos.biomescanner.map.Zone;
 
 @Mod(modid = BiomeScanner.MODID, name = BiomeScanner.MODNAME, version = BiomeScanner.VERSION, dependencies = BiomeScanner.DEPS)
 public class BiomeScanner {
-
+	
 	public static final String MODID = "biomescanner";
 	public static final String MODNAME = "Biome Scanner";
-	public static final String VERSION = "1.2.19";
+	public static final String VERSION = "2.0.5";
 	public static final String DEPS = "before:UndergroundBiomes;after:ThermalFoundation;after:appliedenergistics2;after:Thaumcraft";
 	public static final String COMMONPROXYLOCATION = "talonos." + MODID + ".CommonProxy";
 	public static final String CLIENTPROXYLOCATION = "talonos." + MODID + ".ClientProxy";
-
+	
 	public static File baselineFile;
-
+	
 	public static final CreativeTabs badgesTab = new CreativeTabs("badges") {
 		private static final String __OBFID = "CL_00000011";
-
+		
 		@Override
 		@SideOnly(Side.CLIENT)
 		public Item getTabIconItem() { return BSItems.badge; }
 	};
-
+	
 	@SidedProxy(clientSide = BiomeScanner.CLIENTPROXYLOCATION, serverSide = BiomeScanner.COMMONPROXYLOCATION)
 	public static CommonProxy proxy;
-
+	
 	@Mod.Instance(BiomeScanner.MODID)
 	public static BiomeScanner instance;
-
+	
 	public static Integer[] zoneBaselines = new Integer[Zone.values().length];
-
+	
 	public static boolean disableEverything = false;
-
+	
 	@Mod.EventHandler
 	public static void preInit(FMLPreInitializationEvent event) {
 		BSBlock.init();
 		BSItems.init();
 		proxy.registerTileEntities();
-
-		File configFile = event.getSuggestedConfigurationFile();
-		Configuration config = new Configuration(configFile);
-
-		boolean useBaselineFile = config.get(Configuration.CATEGORY_GENERAL, "useBaselineFile", false).getBoolean();
-		String baselineFileName = config.get(Configuration.CATEGORY_GENERAL, "baselineFileName", "baseline.dat").getString();
+		
+		final File configFile = event.getSuggestedConfigurationFile();
+		final Configuration config = new Configuration(configFile);
+		
+		final boolean useBaselineFile = config.get(Configuration.CATEGORY_GENERAL, "useBaselineFile", false)
+				.getBoolean();
+		final String baselineFileName = config.get(Configuration.CATEGORY_GENERAL, "baselineFileName", "baseline.dat")
+				.getString();
 		disableEverything = config.get(Configuration.CATEGORY_GENERAL, "disableEverything", false).getBoolean();
 		config.save();
-
-		for (int i = 0; i < zoneBaselines.length; i++) {
-			zoneBaselines[i] = null;
-		}
-
+		
+		Arrays.fill(zoneBaselines, null);
+		
 		if (useBaselineFile) {
 			BiomeScanner.baselineFile = new File(configFile.getParent(), baselineFileName);
 			if (baselineFile.exists()) {
 				try {
-					NBTTagCompound loadedData = CompressedStreamTools.readCompressed(new FileInputStream(baselineFile));
+					final NBTTagCompound loadedData = CompressedStreamTools
+							.readCompressed(new FileInputStream(baselineFile));
 					loadBaselineData(loadedData);
-				} catch (IOException ex) {
+				}
+				catch (final IOException ex) {
 					ex.printStackTrace();
 				}
 			}
 		}
 	}
-
+	
 	private static void loadBaselineData(NBTTagCompound loadedData) {
-		NBTTagCompound regionMap = loadedData.getCompoundTag("RegionMap");
-		NBTTagCompound baseline = regionMap.getCompoundTag("BaselineBlocks");
-
+		final NBTTagCompound regionMap = loadedData.getCompoundTag("RegionMap");
+		final NBTTagCompound baseline = regionMap.getCompoundTag("BaselineBlocks");
+		
 		for (int i = 0; i < zoneBaselines.length; i++) {
 			if (baseline.hasKey(Integer.toString(i))) {
 				zoneBaselines[i] = baseline.getInteger(Integer.toString(i));
@@ -101,24 +104,24 @@ public class BiomeScanner {
 				zoneBaselines[i] = null;
 			}
 		}
-
+		
 	}
-
+	
 	@Mod.EventHandler
 	public void init(FMLInitializationEvent event) {
 		FMLCommonHandler.instance().bus().register(MapScanner.instance);
 		MinecraftForge.EVENT_BUS.register(MapScanner.instance);
-
+		
 		ForgeChunkManager.setForcedChunkLoadingCallback(this, MapScanner.instance); // adds chunkloader to forge
 	}
-
+	
 	@Mod.EventHandler
 	public static void postInit(FMLPostInitializationEvent event) {
 		BiomeMapColors.initColors();
 		proxy.registerRenderers();
 		NetworkRegistry.INSTANCE.registerGuiHandler(instance, new GuiHandlerBadgePrinter());
 	}
-
+	
 	@Mod.EventHandler
 	public static void serverLoad(FMLServerStartingEvent event) {
 		event.registerServerCommand(new ResetBaselineCommand());
--- src/main/java/talonos/blightbuster/BlightBuster.java
+++ src/main/java/talonos/blightbuster/BlightBuster.java
@@ -1,10 +1,5 @@
 package talonos.blightbuster;
 
-import java.io.File;
-import java.io.IOException;
-
-import org.apache.commons.io.FileUtils;
-
 import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.SidedProxy;
@@ -31,75 +26,46 @@ import thaumicenergistics.api.ThEApi;
 
 @Mod(modid = BlightBuster.MODID, name = BlightBuster.MODNAME, version = BlightBuster.VERSION, dependencies = BlightBuster.DEPS)
 public class BlightBuster {
-
+	
 	public static final String MODID = "blightbuster";
 	public static final String MODNAME = "BlightBuster";
-	public static final String VERSION = "2.0.3";
+	public static final String VERSION = "2.0.5";
 	public static final String DEPS = "before:UndergroundBiomes;after:ThermalFoundation;after:appliedenergistics2;after:Thaumcraft";
 	public static final String COMMONPROXYLOCATION = "talonos." + MODID + ".CommonProxy";
 	public static final String CLIENTPROXYLOCATION = "talonos." + MODID + ".ClientProxy";
-
+	
 	@SidedProxy(clientSide = BlightBuster.CLIENTPROXYLOCATION, serverSide = BlightBuster.COMMONPROXYLOCATION)
 	public static CommonProxy proxy;
-
+	
 	public static BlightBuster instance;
 	private DawnMachineChunkLoader dawnMachineChunkLoader;
-
+	
 	@Mod.EventHandler
 	public void preInit(FMLPreInitializationEvent event) {
-		
-		// Clean up old TerrainControl configs
-		File check = new File("terraincontrol.dat");
-		if (!check.exists()) {
-			File saves = new File("saves");
-			for (File directory : saves.listFiles() ) {
-				if (directory.isDirectory()) {
-					File tc = new File(new File(directory, "TerrainControl"), "WorldBiomes");
-					try {
-						System.out.println("Deleting " + tc.getCanonicalPath());
-					} catch (IOException e1) {
-						System.out.println("Failed to delete " + tc.getName());
-					}
-					try {
-						FileUtils.deleteDirectory(tc);
-					} catch (IOException e) {
-						// TODO Auto-generated catch block
-						System.out.println("Failed to delete " + tc.getName());
-					}
-				}
-			}
-			try {
-				check.createNewFile();
-			} catch (IOException e) {
-				System.out.println("Failed to create terraincontrol.dat");
-				e.printStackTrace();
-			}
-		}
-		
 		instance = this;
 		BBBlock.init();
 		BBItems.init();
 		proxy.registerTileEntities();
 		EntityRegistry.registerModEntity(EntitySilverPotion.class, "silverPotion", 0, MODID, 250, 5, true);
 	}
-
+	
 	@Mod.EventHandler
 	public void init(FMLInitializationEvent event) {
 		BlightbusterNetwork.init();
-
+		
 		MinecraftForge.EVENT_BUS.register(new PurityFocusEventHandler()); // adds event handler
 		FMLCommonHandler.instance().bus().register(new PurityFocusEventHandler());
-		FoodHandler foodHandler = new FoodHandler();
-		MooshroomSpawnEventHandler spawnHandler = new MooshroomSpawnEventHandler();
+		final FoodHandler foodHandler = new FoodHandler();
+		final MooshroomSpawnEventHandler spawnHandler = new MooshroomSpawnEventHandler();
 		this.dawnMachineChunkLoader = new DawnMachineChunkLoader(); // creates chunk loader
 		ForgeChunkManager.setForcedChunkLoadingCallback(this, this.dawnMachineChunkLoader); // adds chunkloader to forge
-
+		
 		if (ThEApi.instance() != null) {
 			ThEApi.instance().transportPermissions() // adds Dawn Machine spout entity to Thaumic Energistics
 					.addAspectContainerTileToInjectPermissions(DawnMachineSpoutTileEntity.class, 32);
 		}
 	}
-
+	
 	@Mod.EventHandler
 	public void postInit(FMLPostInitializationEvent event) {
 		AddedResearch.initResearch();
@@ -108,9 +74,9 @@ public class BlightBuster {
 		if (CustomItems.blood instanceof BlockBlood) { // take account for config being disabled
 			CustomItems.blood.setHardness(.5f);
 		}
-
+		
 	}
-
+	
 	@Mod.EventHandler
 	public static void serverLoad(FMLServerStartingEvent event) {}
 }
