f1c855cca730a3e560ba86bfabe880369a67442e
Midnight <23142346+Midnight145@users.noreply.github.com>
2024-01-10 15:03:19 -0600

    formatting

--- src/main/java/talonos/blightbuster/AddedResearch.java
+++ src/main/java/talonos/blightbuster/AddedResearch.java
@@ -1,9 +1,8 @@
 package talonos.blightbuster;
 
-import net.minecraft.init.Blocks;
+import cofh.thermalexpansion.block.TEBlocks;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
-import cofh.thermalexpansion.block.TEBlocks;
 import talonos.blightbuster.blocks.BBBlock;
 import talonos.blightbuster.handlers.TalonosWandTriggerManager;
 import talonos.blightbuster.items.BBItems;
@@ -17,8 +16,6 @@ import thaumcraft.api.research.ResearchPage;
 import thaumcraft.api.wands.WandTriggerRegistry;
 import thaumcraft.common.config.ConfigBlocks;
 import thaumcraft.common.config.ConfigItems;
-import tuhljin.automagy.items.ModItems;
-import fox.spiteful.forbidden.DarkAspects;
 
 public class AddedResearch {
 	
@@ -39,22 +36,14 @@ public class AddedResearch {
 		
 		// Creates the silverleaf potion recipe
 		ShapedArcaneRecipe silverPotRecipe = ThaumcraftApi.addArcaneCraftingRecipe("SILVERPOTION",
-				new ItemStack(BBItems.silverPotion, 6), new AspectList().add(Aspect.WATER, 15).add(Aspect.ORDER, 24), // aspects,
-																														 // requires
-																														 // 15
-																														 // aqua
-																														 // and
-																														 // 24
-																														 // ordo
-				new Object[] { "FLF", // recipe pattern
-						'L', new ItemStack(ConfigBlocks.blockMagicalLeaves, 1, 1), // Silverwood leaves
-						'F', new ItemStack(Items.glass_bottle) // glass bottle
-				});
-				
+				new ItemStack(BBItems.silverPotion, 6), new AspectList().add(Aspect.WATER, 15).add(Aspect.ORDER, 24),
+				"FLF", 'L', new ItemStack(ConfigBlocks.blockMagicalLeaves, 1, 1), 'F',
+				new ItemStack(Items.glass_bottle));
+		
 		ShapedArcaneRecipe purityFocusRecipe = ThaumcraftApi.addArcaneCraftingRecipe("PURITYFOCUS",
-				new ItemStack(BBItems.purityFocus), new AspectList().add(Aspect.WATER, 5).add(Aspect.ORDER, 8),
-				new Object[] { "SQS", "QFQ", "SQS", 'S', new ItemStack(ConfigItems.itemShard, 1, 4), 'Q',
-						new ItemStack(Items.quartz), 'F', new ItemStack(ConfigBlocks.blockCustomPlant, 1, 4) });
+				new ItemStack(BBItems.purityFocus), new AspectList().add(Aspect.WATER, 5).add(Aspect.ORDER, 8), "SQS",
+				"QFQ", "SQS", 'S', new ItemStack(ConfigItems.itemShard, 1, 4), 'Q', new ItemStack(Items.quartz), 'F',
+				new ItemStack(ConfigBlocks.blockCustomPlant, 1, 4));
 		
 		InfusionRecipe dawnTotemRecipe = ThaumcraftApi.addInfusionCraftingRecipe("DAWNTOTEM",
 				new ItemStack(BBBlock.dawnTotem), 6, new AspectList().add(Aspect.AURA, 16).add(Aspect.HEAL, 32)
@@ -67,61 +56,45 @@ public class AddedResearch {
 						new ItemStack(ConfigBlocks.blockCosmeticSolid, 1, 3),
 						new ItemStack(ConfigBlocks.blockCrystal, 1, 4) });
 		
-		InfusionRecipe emberHeartRecipe = ThaumcraftApi.addInfusionCraftingRecipe("NETHERMIND",
-				new ItemStack(ModItems.netherHeart), 6, new AspectList().add(DarkAspects.NETHER, 16)
-						.add(Aspect.EXCHANGE, 16).add(Aspect.MAGIC, 32).add(DarkAspects.WRATH, 16),
-				new ItemStack(Items.ghast_tear), // center item
-				new ItemStack[] { // items in recipe
-						new ItemStack(Items.blaze_rod), new ItemStack(Items.blaze_rod), new ItemStack(Items.blaze_rod),
-						new ItemStack(Items.blaze_rod), new ItemStack(Blocks.netherrack),
-						new ItemStack(Blocks.netherrack), new ItemStack(Blocks.obsidian),
-						new ItemStack(Blocks.obsidian), new ItemStack(Items.leather) });
-		
 		ShapedArcaneRecipe dawnChargerRecipe = ThaumcraftApi.addArcaneCraftingRecipe("DAWNCHARGER",
 				new ItemStack(BBBlock.dawnCharger),
 				new AspectList().add(Aspect.WATER, 25).add(Aspect.ORDER, 25).add(Aspect.ENTROPY, 25)
 						.add(Aspect.FIRE, 25).add(Aspect.EARTH, 25).add(Aspect.AIR, 25),
-				new Object[] { "SSS", "SLS", "SSS", 'S', new ItemStack(ConfigBlocks.blockMagicalLog, 1, 1), 'L',
-						new ItemStack(TEBlocks.blockFrame, 1, 7), });
+				"SSS", "SLS", "SSS", 'S', new ItemStack(ConfigBlocks.blockMagicalLog, 1, 1), 'L',
+				new ItemStack(TEBlocks.blockFrame, 1, 7));
 		
 		ResearchItem silverPotResearch = new ResearchItem("SILVERPOTION", // research name
 				"ALCHEMY", // thaumonomicon page
-				new AspectList().add(Aspect.WATER, 3).add(Aspect.HEAL, 6).add(Aspect.TAINT, 3).add(Aspect.PLANT, 3), // aspects
-																													 // in
-																													 // research
-				-1, -4, 2, // location in thaumonomicon
+				new AspectList().add(Aspect.WATER, 3).add(Aspect.HEAL, 6).add(Aspect.TAINT, 3).add(Aspect.PLANT, 3), -1,
+				-4, 2, // location in thaumonomicon
 				new ItemStack(BBItems.silverPotion)); // research icon
 		
-		silverPotResearch.setPages(new ResearchPage[] { new ResearchPage("tc.research_page.SILVERPOTION.1"), // page in
-																											 // language
-																											 // file
-				new ResearchPage(silverPotRecipe) // page for recipe
-		});
+		silverPotResearch.setPages(new ResearchPage("tc.research_page.SILVERPOTION.1"),
+				new ResearchPage(silverPotRecipe));
 		
 		silverPotResearch.setConcealed(); // sets as hidden in thaumonomicon
-		silverPotResearch.setParents(new String[] { "ETHEREALBLOOM" }); // sets requirements for research
+		silverPotResearch.setParents("ETHEREALBLOOM"); // sets requirements for research
 		silverPotResearch.registerResearchItem(); // actually registers it
 		
 		ResearchItem purityFocusResearch = new ResearchItem("PURITYFOCUS", "ALCHEMY",
 				new AspectList().add(Aspect.TOOL, 3).add(Aspect.HEAL, 6).add(Aspect.TAINT, 3).add(Aspect.MAGIC, 3), -3,
 				-4, 2, new ItemStack(BBItems.purityFocus));
 		
-		purityFocusResearch.setPages(new ResearchPage[] { new ResearchPage("tc.research_page.PURITYFOCUS.1"),
-				new ResearchPage(purityFocusRecipe) });
+		purityFocusResearch.setPages(new ResearchPage("tc.research_page.PURITYFOCUS.1"),
+				new ResearchPage(purityFocusRecipe));
 		
 		purityFocusResearch.setConcealed();
-		purityFocusResearch.setParents(new String[] { "ETHEREALBLOOM" });
+		purityFocusResearch.setParents("ETHEREALBLOOM");
 		purityFocusResearch.registerResearchItem();
 		
 		ResearchItem dawnTotemResearch = new ResearchItem("DAWNTOTEM", "ALCHEMY",
 				new AspectList().add(Aspect.AURA, 6).add(Aspect.HEAL, 8).add(Aspect.TAINT, 3).add(Aspect.MAGIC, 6), -2,
 				-6, 2, new ItemStack(BBBlock.dawnTotem));
 		
-		dawnTotemResearch.setPages(new ResearchPage[] { new ResearchPage("tc.research_page.DAWNTOTEM.1"),
-				new ResearchPage(dawnTotemRecipe) });
+		dawnTotemResearch.setPages(new ResearchPage("tc.research_page.DAWNTOTEM.1"), new ResearchPage(dawnTotemRecipe));
 		
 		dawnTotemResearch.setConcealed();
-		dawnTotemResearch.setParents(new String[] { "SILVERPOTION", "PURITYFOCUS" });
+		dawnTotemResearch.setParents("SILVERPOTION", "PURITYFOCUS");
 		dawnTotemResearch.registerResearchItem();
 		
 		ResearchItem dawnChargerResearch = new ResearchItem(
@@ -129,11 +102,11 @@ public class AddedResearch {
 						.add(Aspect.MECHANISM, 8).add(Aspect.ENERGY, 4).add(Aspect.MAGIC, 8),
 				-4, -7, 2, new ItemStack(BBBlock.dawnCharger));
 		
-		dawnChargerResearch.setPages(new ResearchPage[] { new ResearchPage("tc.research_page.DAWNCHARGER.1"),
-				new ResearchPage(dawnChargerRecipe) });
+		dawnChargerResearch.setPages(new ResearchPage("tc.research_page.DAWNCHARGER.1"),
+				new ResearchPage(dawnChargerRecipe));
 		
 		dawnChargerResearch.setConcealed();
-		dawnChargerResearch.setParents(new String[] { "DAWNOFFERING" });
+		dawnChargerResearch.setParents("DAWNOFFERING");
 		dawnChargerResearch.registerResearchItem();
 		
 		TalonosWandTriggerManager wandTrigger = new TalonosWandTriggerManager();
