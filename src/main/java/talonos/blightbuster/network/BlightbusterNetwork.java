b313535636a9f70cb6f1ff09ec5b8efc4d19b584
Midnight <23142346+Midnight145@users.noreply.github.com>
2024-04-19 14:24:15 -0500

    move to it's own mod

--- src/main/java/talonos/blightbuster/network/BlightbusterNetwork.java
+++ src/main/java/talonos/blightbuster/network/BlightbusterNetwork.java
@@ -6,22 +6,16 @@ import net.minecraft.world.biome.BiomeGenBase;
 import net.minecraft.world.chunk.Chunk;
 
 import cpw.mods.fml.common.FMLCommonHandler;
-import cpw.mods.fml.common.eventhandler.SubscribeEvent;
-import cpw.mods.fml.common.gameevent.PlayerEvent;
 import cpw.mods.fml.common.network.NetworkRegistry;
 import cpw.mods.fml.common.network.simpleimpl.IMessage;
 import cpw.mods.fml.common.network.simpleimpl.SimpleNetworkWrapper;
 import cpw.mods.fml.relauncher.Side;
 import io.netty.channel.ChannelHandler;
-import talonos.biomescanner.map.MapScanner;
-import talonos.biomescanner.map.event.UpdateMapEvent;
 import talonos.blightbuster.BlightBuster;
 import talonos.blightbuster.network.handlers.BiomeChangeHandler;
 import talonos.blightbuster.network.handlers.SpawnCleanseParticleHandler;
-import talonos.blightbuster.network.handlers.UpdateClientMapHandler;
 import talonos.blightbuster.network.packets.BiomeChangePacket;
 import talonos.blightbuster.network.packets.SpawnCleanseParticlesPacket;
-import talonos.blightbuster.network.packets.UpdateMapPacket;
 
 @ChannelHandler.Sharable
 public class BlightbusterNetwork {
@@ -33,42 +27,11 @@ public class BlightbusterNetwork {
         INSTANCE.networkWrapper = NetworkRegistry.INSTANCE.newSimpleChannel(BlightBuster.MODID);
         INSTANCE.networkWrapper
             .registerMessage(SpawnCleanseParticleHandler.class, SpawnCleanseParticlesPacket.class, 0, Side.CLIENT);
-        INSTANCE.networkWrapper.registerMessage(UpdateClientMapHandler.class, UpdateMapPacket.class, 1, Side.CLIENT);
         INSTANCE.networkWrapper.registerMessage(BiomeChangeHandler.class, BiomeChangePacket.class, 2, Side.CLIENT);
-
-        if (FMLCommonHandler.instance()
-            .getEffectiveSide()
-            .isServer()) {
-            MapScanner.instance.bus()
-                .register(INSTANCE);
-        }
 
         FMLCommonHandler.instance()
             .bus()
             .register(INSTANCE);
-    }
-
-    @SubscribeEvent
-    public void onUpdateMap(UpdateMapEvent event) {
-        int minX = event.getX();
-        int minY = event.getY();
-        int width = event.getWidth();
-        int height = event.getHeight();
-
-        byte[] data = new byte[width * height];
-        for (int y = 0; y < height; y++) {
-            for (int x = 0; x < width; x++) {
-                data[(y * width) + x] = MapScanner.instance.getRawColorByte(x + minX, y + minY);
-            }
-        }
-        sendToAllPlayers(new UpdateMapPacket(minX, minY, width, height, data));
-    }
-
-    @SubscribeEvent
-    public void onPlayerLoggedIn(PlayerEvent.PlayerLoggedInEvent event) {
-        if (event.player instanceof EntityPlayerMP) {
-            MapScanner.instance.sendEntireMap((EntityPlayerMP) event.player);
-        }
     }
 
     public static void sendToAllPlayers(IMessage packet) {
@@ -77,10 +40,6 @@ public class BlightbusterNetwork {
 
     public static void sendToNearbyPlayers(IMessage message, int dimension, float x, float y, float z, float radius) {
         INSTANCE.networkWrapper.sendToAllAround(message, new NetworkRegistry.TargetPoint(dimension, x, y, z, radius));
-    }
-
-    public static void sendToPlayer(IMessage message, EntityPlayerMP player) {
-        INSTANCE.networkWrapper.sendTo(message, player);
     }
 
     public static void setBiomeAt(World world, int x, int z, BiomeGenBase biome) {
