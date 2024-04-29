07621fc6efa4f71620b07b1125263a6ed283ab27
Midnight <23142346+Midnight145@users.noreply.github.com>
2024-04-29 02:59:05 -0500

    start move to config framework + move handlers to blighttweaks

--- src/main/java/talonos/blightbuster/BlightBuster.java
+++ src/main/java/talonos/blightbuster/BlightBuster.java
@@ -2,6 +2,10 @@ package talonos.blightbuster;
 
 import net.minecraftforge.common.ForgeChunkManager;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.common.config.Configuration;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.common.Mod;
@@ -10,13 +14,8 @@ import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.registry.EntityRegistry;
-import noppes.npcs.CustomItems;
-import noppes.npcs.blocks.BlockBlood;
 import talonos.blightbuster.blocks.BBBlock;
 import talonos.blightbuster.entities.EntitySilverPotion;
-import talonos.blightbuster.handlers.ChunkHandler;
-import talonos.blightbuster.handlers.FoodHandler;
-import talonos.blightbuster.handlers.InteractHandler;
 import talonos.blightbuster.handlers.PurityFocusEventHandler;
 import talonos.blightbuster.items.BBItems;
 import talonos.blightbuster.network.BlightbusterNetwork;
@@ -42,33 +41,42 @@ public class BlightBuster {
     @SidedProxy(clientSide = BlightBuster.CLIENTPROXYLOCATION, serverSide = BlightBuster.COMMONPROXYLOCATION)
     public static CommonProxy proxy;
 
+    public static Logger logger = LogManager.getLogger(BlightBuster.MODID);
+
     public static BlightBuster instance;
     private DawnMachineChunkLoader dawnMachineChunkLoader;
 
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event) {
+        Configuration config = new Configuration(event.getSuggestedConfigurationFile());
+        BlightbusterConfig.load(config);
         instance = this;
         BBBlock.init();
         BBItems.init();
         proxy.registerTileEntities();
-        EntityRegistry.registerModEntity(EntitySilverPotion.class, "silverPotion", 0, MODID, 250, 5, true);
+        if (BlightbusterConfig.enableSilverPotion) {
+            EntityRegistry.registerModEntity(EntitySilverPotion.class, "silverPotion", 0, MODID, 250, 5, true);
+        }
     }
 
     @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
         BlightbusterNetwork.init();
 
-        MinecraftForge.EVENT_BUS.register(new PurityFocusEventHandler()); // adds event handler
-        FMLCommonHandler.instance()
-            .bus()
-            .register(new PurityFocusEventHandler());
-        final FoodHandler foodHandler = new FoodHandler();
-        InteractHandler interactHandler = new InteractHandler();
-        ChunkHandler chunkHandler = new ChunkHandler();
-        this.dawnMachineChunkLoader = new DawnMachineChunkLoader(); // creates chunk loader
-        ForgeChunkManager.setForcedChunkLoadingCallback(this, this.dawnMachineChunkLoader); // adds chunkloader to forge
+        if (BlightbusterConfig.enablePurityFocus) {
+            MinecraftForge.EVENT_BUS.register(new PurityFocusEventHandler()); // adds event handler
+            FMLCommonHandler.instance()
+                .bus()
+                .register(new PurityFocusEventHandler());
+        }
 
-        if (ThEApi.instance() != null) {
+        if (BlightbusterConfig.enableDawnMachine) {
+            this.dawnMachineChunkLoader = new DawnMachineChunkLoader(); // creates chunk loader
+            ForgeChunkManager.setForcedChunkLoadingCallback(this, this.dawnMachineChunkLoader); // adds chunkloader to
+                                                                                                // forge
+        }
+
+        if (BlightbusterConfig.enableThaumicEnergistics && ThEApi.instance() != null) {
             ThEApi.instance()
                 .transportPermissions() // adds Dawn Machine spout entity to Thaumic Energistics
                 .addAspectContainerTileToInjectPermissions(DawnMachineSpoutTileEntity.class, 320);
@@ -79,10 +87,10 @@ public class BlightBuster {
     public void postInit(FMLPostInitializationEvent event) {
         AddedResearch.initResearch();
         proxy.registerRenderers();
-        RitualDawnMachine.init();
-        if (CustomItems.blood instanceof BlockBlood) { // take account for config being disabled
-            CustomItems.blood.setHardness(.5f);
+        if (BlightbusterConfig.enableBlood) {
+            RitualDawnMachine.init();
         }
+        BlightbusterConfig.init_mappings();
 
     }
 }
