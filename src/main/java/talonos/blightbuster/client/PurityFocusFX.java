532bd0455c61207f02d101bd8532ee29c982f54c
koolkrafter5 <creepersrgreen@gmail.com>
2025-03-04 10:22:50 -0500

    Fix some particle effect code

--- src/main/java/talonos/blightbuster/client/PurityFocusFX.java
+++ src/main/java/talonos/blightbuster/client/PurityFocusFX.java
@@ -16,9 +16,6 @@ public class PurityFocusFX {
     public static HashMap<UUID, Long> lastNodeTime = new HashMap<>();
 
     public static void fire(EntityLivingBase entity, EntityPlayer player) {
-        double w;
-        double d;
-        double h;
         // Only play this sound once per cast, even if multiple entities are hit
         if (!lastFireTime.containsKey(player.getUniqueID())
             || lastFireTime.get(player.getUniqueID()) < entity.worldObj.getTotalWorldTime() - 4) {
@@ -27,9 +24,9 @@ public class PurityFocusFX {
         }
         double count = 3 + entity.worldObj.rand.nextInt(8) * entity.height;
         for (int i = (int) count; i >= 0; i--) {
-            w = entity.worldObj.rand.nextGaussian() * entity.width * 0.6;
-            d = entity.worldObj.rand.nextGaussian() * entity.width * 0.6;
-            h = entity.worldObj.rand.nextFloat() * entity.height * 0.05F + entity.height * (i / count);
+            double w = entity.worldObj.rand.nextGaussian() * entity.width * 0.6;
+            double d = entity.worldObj.rand.nextGaussian() * entity.width * 0.6;
+            double h = entity.worldObj.rand.nextFloat() * entity.height * 0.05F + entity.height * (i / count);
             Thaumcraft.proxy.drawGenericParticles(
                 entity.worldObj,
                 entity.posX + w,
@@ -53,29 +50,25 @@ public class PurityFocusFX {
     }
 
     public static void clean(EntityLivingBase entity, EntityPlayer player) {
-        double w;
-        double d;
-        double h;
         // Only play this sound once per cast, even if multiple entities are hit
         if (!lastCleanTime.containsKey(player.getUniqueID())
             || lastCleanTime.get(player.getUniqueID()) < entity.worldObj.getTotalWorldTime() - 4) {
-            entity.worldObj.playSound(entity.posX, entity.posY, entity.posZ, "thaumcraft:gore", 0.5F, 1, false);
+            entity.worldObj.playSound(entity.posX, entity.posY, entity.posZ, "thaumcraft:gore", 0.75F, 1, false);
             lastCleanTime.put(player.getUniqueID(), entity.worldObj.getTotalWorldTime());
         }
-        for (int i = 20; i > 0; i--) {
-            for (int j = 20; j > 0; j--) {
-                w = entity.worldObj.rand.nextFloat() * entity.width;
-                d = entity.worldObj.rand.nextFloat() * entity.width;
-                h = entity.worldObj.rand.nextFloat() * entity.height;
+        for (int i = 10; i > 0; i--) {
+            double w = entity.worldObj.rand.nextGaussian() * entity.width * 0.5;
+            double d = entity.worldObj.rand.nextGaussian() * entity.width * 0.5;
+            double h = entity.worldObj.rand.nextFloat() * entity.height * 0.8F;
 
-            }
+            double d0 = entity.worldObj.rand.nextGaussian() * 0.02D;
+            double d1 = entity.worldObj.rand.nextGaussian() * 0.02D;
+            double d2 = entity.worldObj.rand.nextGaussian() * 0.02D;
+            entity.worldObj.spawnParticle("witchMagic", entity.posX + w, entity.posY + h, entity.posZ + d, d0, d1, d2);
         }
     }
 
     public static void heal(EntityLivingBase entity, EntityPlayer player) {
-        double w;
-        double d;
-        double h;
         // Only play this sound once per cast, even if multiple entities are hit
         if (!lastHealTime.containsKey(player.getUniqueID())
             || lastHealTime.get(player.getUniqueID()) < entity.worldObj.getTotalWorldTime() - 4) {
@@ -83,9 +76,9 @@ public class PurityFocusFX {
             lastHealTime.put(player.getUniqueID(), entity.worldObj.getTotalWorldTime());
         }
         for (int i = (2 + entity.worldObj.rand.nextInt(3)); i > 0; i--) {
-            w = (entity.worldObj.rand.nextGaussian() - 0.5) * entity.width;
-            d = (entity.worldObj.rand.nextGaussian() - 0.5) * entity.width;
-            h = (entity.worldObj.rand.nextGaussian() - 0.2) * entity.height * 0.8F;
+            double w = entity.worldObj.rand.nextGaussian() * entity.width;
+            double d = entity.worldObj.rand.nextGaussian() * entity.width;
+            double h = (entity.worldObj.rand.nextGaussian() - 0.4) * entity.height * 0.8F;
 
             double d0 = entity.worldObj.rand.nextGaussian() * 0.02D;
             double d1 = entity.worldObj.rand.nextGaussian() * 0.02D;
@@ -95,22 +88,19 @@ public class PurityFocusFX {
     }
 
     public static void node(EntityPlayer player, int x, int y, int z) {
-        float w;
-        float d;
-        float h;
         // Only play this sound once at a time, even if multiple nodes are hit.
         // It also only plays if the node is within 10 blocks.
         if (!lastNodeTime.containsKey(player.getUniqueID())
             || lastNodeTime.get(player.getUniqueID()) < player.worldObj.getTotalWorldTime() - 4
                 && player.getDistanceSq(x, y, z) < 100) {
-            player.playSound("thaumcraft:wand", 0.5F, 1F);
+            player.worldObj.playSound(player.posX, player.posY, player.posZ, "thaumcraft:wand", 0.5F, 1, false);
             lastNodeTime.put(player.getUniqueID(), player.worldObj.getTotalWorldTime());
         }
 
         for (int i = 12; i >= 0; i--) {
-            w = (float) (player.worldObj.rand.nextGaussian() + 1) / 2F;
-            d = (float) (player.worldObj.rand.nextGaussian() + 1) / 2F;
-            h = (float) (player.worldObj.rand.nextGaussian() + 1) / 2F;
+            float w = (float) (player.worldObj.rand.nextGaussian() + 1) / 2F;
+            float d = (float) (player.worldObj.rand.nextGaussian() + 1) / 2F;
+            float h = (float) (player.worldObj.rand.nextGaussian() + 1) / 2F;
             Thaumcraft.proxy.sparkle(x + w, y + d, z + h, 2, 0, 0);
         }
     }
